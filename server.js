5f537d11dc62d6e959425efe86ebc697c7a9f656
Brannon Lucas <brannonlucas@gmail.com>
2016-02-27 16:47:23 -0700

    Update order forms

--- server.js
+++ server.js
@@ -1,31 +1,49 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
-var AuthorizeNet = require('authorize-net');
-// var payments = new AuthorizeNet({
-//   API_LOGIN_ID: process.env.AUTHORIZE_NET_API_LOGIN,
-//   TRANSACTION_KEY: process.env.AUTHORIZE_NET_TRANSACTION_KEY
-// });
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var AuthorizeNet = require('authorize-net');
+var payments = new AuthorizeNet({
+	API_LOGIN_ID: process.env.AUTHORIZE_NET_API_LOGIN,
+	TRANSACTION_KEY: process.env.AUTHORIZE_NET_TRANSACTION_KEY
+});
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/api/order', urlencodedParser, function(req, res) {
-	console.log(req.body);
-	var order = {};
-	var creditCard = {};
-	var prospect = {};
+app.post('/api/order', function(req, res) {
+	var details = req.body;
+	console.log(details);
+	var order = {
+		amount: details.amount
+	};
+	var creditCard = {
+		creditCardNumber: details.creditCardNumber,
+		expirationMonth: details.expirationMonth,
+		expirationYear: details.expirationYear,
+		cvv: details.cvv
+	};
+	var prospect = {
+		customerFirstName: details.customerFirstName,
+		customerLastName: details.customerLastName,
+		customerEmail: details.customerEmail,
+		billingAddress: details.billingAddress,
+		billingCity: details.billingCity,
+		billingState: details.billingState,
+		billingZip: details.billingZip,
+		billingCountry: details.billingCountry
+	};
 
-	res.redirect('/');
-
-	// payments.authorizeTransaction(order, creditCard, prospect).then(function(res) {
-	// 	res.send({
-	// 		transactionId: res.transactionId,
-	// 		message: 'Thank you for you purchase. You will receive an email for you records.'
-	// 	});
-	// }).catch(res.send);
+	authNet.submitTransaction(order, creditCard, prospect).then(function(response) {
+		console.log(response);
+		// res.send({
+		// 	transactionId: res.transactionId,
+		// 	message: 'Thank you for you purchase. You will receive an email for you records.'
+		// });
+	});
 });
 
 app.listen(port, function() {
